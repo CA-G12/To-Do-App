e7fa421af4bedd6e2dc7644e3e14f61f548448d4
hkmusameh01 <abdalhakem1226@gmail.com>
2022-08-23 16:52:27 +0300

    creating server on port 5050 Relates #5

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const { join } = require("path");
+
+const express = require("express");
+
+const compression = require("compression");
+const helmet = require("helmet");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(compression());
+
+app.use(express.static(join(__dirname, "..", "public")));
+
+module.exports = app;
