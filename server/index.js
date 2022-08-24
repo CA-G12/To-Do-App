6c3ca0653dff156fc8bb91bebe43c164a2e547cc
Bakeza <dbakeza2002@gmail.com>
2022-08-24 15:30:59 +0300

    Fix port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 const app = require('./app');
 
-app.set('port', 5050);
-
 app.listen(app.get('port'), () => {
   console.log(`Your server is running on port http://localhost:${app.get('port')}`);
 })
